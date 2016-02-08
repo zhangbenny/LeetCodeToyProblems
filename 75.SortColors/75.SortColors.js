e11fde9241c269fb9406352cab1084574c0fbcfb
Benny Zhang <zhang.benny@gmail.com>
2016-02-07 22:09:19 -0800

    Quicksort implementation doesn't mutate original array

--- 75.SortColors/75.SortColors.js
+++ 75.SortColors/75.SortColors.js
@@ -1,26 +1,39 @@
-var sortColors = function(nums) {
-  function recurse (array) {
-    if (array.length < 1) {
-      return [];
-    }
-    var pivot = Math.floor(Math.random() * array.length);
-    var equal = [];
-    var less = [];
-    var greater = [];
-    for (var i = 0; i < array.length; i++) {
-      if (array[i] === array[pivot]) {
-        equal.push(array[i]);
-      }
-      if (array[i] < array[pivot]) {
-        less.push(array[i]);
-      }
-      if (array[i] > array[pivot]) {
-        greater.push(array[i])
-      }
-    }
-    return [].concat(recurse(less), equal, recurse(greater));
-  }
-  return recurse(nums);
-};
+// var sortColors = function(nums) {
+//   if (nums.length < 1) {
+//     return [];
+//   }
+//   var pivot = Math.floor(Math.random() * nums.length);
+//   var equal = [];
+//   var less = [];
+//   var greater = [];
+//   for (var i = 0; i < nums.length; i++) {
+//     if (nums[i] === nums[pivot]) {
+//       equal.push(nums[i]);
+//     }
+//     if (nums[i] < nums[pivot]) {
+//       less.push(nums[i]);
+//     }
+//     if (nums[i] > nums[pivot]) {
+//       greater.push(nums[i])
+//     }
+//   }
+//   nums = [].concat(sortColors(less), equal, sortColors(greater));
+// };
 
-console.log(sortColors([0,1,2,1,2,1,2,1,1,1,1,2,2,2,1,1,1,1,0]))
+
+
+var sortColors = function(nums) {
+  var changesThisRound = false;
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[i+1]) {
+      var temp = nums[i];
+      nums[i] = nums[i+1];
+      nums[i+1] = temp;
+      changesThisRound = true;
+    }
+  }
+  if (changesThisRound) {
+    sortColors(nums);
+  }
+}
+
